d132bbb18cae6eb0be980fc472ef10d954831747
Sameer <sameer.barha12@gmail.com>
2024-03-02 19:55:00 +0530

    serving route via handler

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 #![allow(unused)]
 
 use axum::response::Html;
+use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::Router;
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route(
+    /*let routes_hello = Router::new().route(
         "/hello",
         get(|| async { Html("Hello<strong> World !!!</strong>") }),
     );
+    */
 
-    //let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    let routes_hello = Router::new().route("/hello", get(handler_hello));
 
     let listener = TcpListener::bind("127.0.0.1:8087").await.unwrap();
     println!("->> LISTENING on {:?}\n", listener.local_addr());
@@ -22,4 +24,10 @@ async fn main() {
     axum::serve(listener, routes_hello.into_make_service())
         .await
         .unwrap();
+}
+
+async fn handler_hello() -> impl IntoResponse {
+    println!("TERMINAL {:<12} - handler_hello", "RESP");
+
+    Html("Hello <string> World !!!<strong>")
 }
